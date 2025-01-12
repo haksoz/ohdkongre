3413c5f70866d1fa52604eeb20666c2d425fb38c
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-01-13 01:36:47 +0300

    Genel Bilgiler içeriği revize edildi.

--- genel-bilgiler.php
+++ genel-bilgiler.php
@@ -8,53 +8,72 @@
 </head>
 
 <body data-spy="scroll" data-target=".navbar-custom" id="page-top">
-    <!-- Preloader  -->
-    <div id="preloader">
-        <div class="spinner">
-            <div class="bounce1"></div>
-            <div class="bounce2"></div>
-            <div class="bounce3"></div>
-        </div>
-    </div><!-- /Preloader ends -->
-    <div id="page-width">
-        <?php include("header.php") ?>
-        <!-- Blog Page -->
-        <!-- Blog Single Page  -->
-        <div id="page-section">
-            <section id="blog">
-                <div class="container-fluid">
-                    <div class="jumbotron"></div>
-                    <div class="jumbo-heading">
-                        <img src="/doc/ohd-banner-inpage.png" alt="">
-                    </div>
+<div id="preloader">
+    <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+</div>
+<div id="page-width">
+    <?php include("header.php") ?>
+    <div id="page-section">
+        <section id="blog">
+            <div class="container-fluid">
+                <div class="jumbotron"></div>
+                <div class="jumbo-heading">
+                    <img src="/doc/ohd-banner-inpage.png" alt="">
                 </div>
-            </section>
-            <section class="container" id="services">
-                <!-- Section heading -->
-                <div class="section-heading">
-                    <h2 class="mt-5">GENEL BİLGİLER</h2><!-- divider -->
-                    <div class="hr"></div>
-                </div>
-                <div class="col-md-12 col-md-12 res-margin">
-                    <p><b class="text-uppercase fs-18 mr-2" style="font-size: 18px;">Toplantı Adı: </b> 1. Uluslararası Onkoloji Hemşireliği Derneği Kongresi</p>
-                    <p><b class="text-uppercase fs-18 mr-2">Toplanı Yeri: </b> Ankara Gazi Üniversitesi Rektörlüğü Mimar Kemalettin Salonu</p>
-                    <p><b class="text-uppercase fs-18 mr-2">Toplantı Tarihi: </b> 21 Mayıs - 23 Mayıs 2025</p>
-                    <p><b class="text-uppercase fs-18 mr-2">Toplantı Konusu: </b> Onkoloji Hemşireliğinde 35 Yıllık Miras Geçmişten Geleceğe</p><br>
-                </div>
-<!--                <div class="col-lg-12 col-md-12">-->
-<!--                    <ul class="custom no-margin">-->
-<!--                        <li>Solo Event, Çocuk Alerji ve Astım Akademisi Derneği tarafından “17. Uluslararası Katılımlı Çocuk Alerji ve Astım Kongresi” resmi organizasyon firması olarak yetkilendirilmiştir.</li>-->
-<!--                        <li>Tüm kayıt - konaklama - sponsorluk faaliyetlerinin pazarlanması ve satışı Solo Event tarafından yürütülecektir.</li>-->
-<!--                    </ul>-->
-<!--                </div>-->
-
-            </section><!-- /section  -->
-        </div><!--/page-section-->
-
-        <?php include("footer.php") ?>
-    </div><!-- /page-width -->
-
-    <?php include("script.php") ?>
+            </div>
+        </section>
+        <section class="container" id="services">
+            <div class="section-heading">
+                <h2 class="mt-5">GENEL BİLGİLER</h2>
+                <div class="hr"></div>
+            </div>
+            <div class="col-md-12 col-md-12 res-margin">
+                <p>
+                    <b class="text-uppercase fs-18 mr-2" style="font-size: 18px;">Toplantı Adı: </b><br>
+                    1. Uluslararası Onkoloji Hemşireliği Derneği Kongresi
+                </p>
+                <p>
+                    <b class="text-uppercase fs-18 mr-2">Toplanı Yeri ve Tarihi: </b> <br>
+                    1. Uluslararası Onkoloji Hemşireliği Derneği Kongresi, 21 - 23 Mayıs 2025 tarihleri arasında Ankara
+                    Gazi Üniversitesi Rektörlüğü Mimar Kemalettin Salonu’nda düzenlenecektir.
+                </p>
+                <p>
+                    <b class="text-uppercase fs-18 mr-2">Toplantı Dili: </b><br>
+                    Kongrenin resmi dili Türkçe’dir.
+                </p>
+                <p>
+                    <b class="text-uppercase fs-18 mr-2">Kayıt ve Danışma Masaları: </b><br>
+                    Ankara Gazi Üniversitesi Rektörlüğü’ndeki kayıt ve danışma masaları 21 Mayıs tarihinden itibaren
+                    çalışmaya başlayacaktır. Bu masalar 21 – 23 Mayıs tarihleri arasında 08:00 – 18:00 saatleri arasında
+                    açık kalacaktır.
+                </p>
+                <p>
+                    <b class="text-uppercase fs-18 mr-2">Yaka Kartı: </b><br>
+                    Tüm katılımcılara yaka kartı verilecektir. Kongre düzeninin sağlanabilmesi için yaka kartlarının
+                    sürekli takılması rica olunur.
+                </p>
+                <p>
+                    <b class="text-uppercase fs-18 mr-2">Katılım Sertifikası: </b><br>
+                    Tüm katılımcılar katılım sertifikalarını 23 Mayıs 2025 tarihinde saat 12:00 itibariyle kayıt
+                    deskinden alabileceklerdir.
+                </p>
+                <p>
+                    <b class="text-uppercase fs-18 mr-2">İzinler: </b><br>
+                    Kongre katılımı için kurumlara verilmek üzere talep edilecek kongre davet yazıları Kongre
+                    Sekreterliği aracılığı ile isteyen katılımcılara gönderilecektir. Bu tür davet yazıları sadece izin
+                    amacı ile kullanılabilir. Bu tür davet mektubu sahibi misafirlerin kayıt ve konaklama ücretleri
+                    kendilerine aittir.
+                </p>
+            </div>
+        </section>
+    </div>
+    <?php include("footer.php") ?>
+</div>
+<?php include("script.php") ?>
 </body>
 
 </html>
