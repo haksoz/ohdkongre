e20241c416792ce885bc6658b6a5f9290c3f7bad
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-01-13 01:37:23 +0300

    Kayıt konaklama bilgileri revize edildi ve tasarım düzenlemesi yapıldı.

--- kayit-konaklama.php
+++ kayit-konaklama.php
@@ -38,85 +38,85 @@
                  <div class="col-md-12 col-md-12 res-margin">
                     <h3 class="text-center mt-5">KAYIT</h3>
                 </div>
-				<div class="col-lg-12 col-md-12">
-                    <h4 class="sub-header">Kayıt</h4>
+                <center>
+                    <div class="col-md-10" style="float: none;">
+                        <!-- Tables -->
+                        <table class="table table-bordered table-striped table-hover ohd-table">
+                            <thead>
+                            <tr>
+                                <th style="">Kayıt Tipi</th>
+                                <th>20 Nisan 2025 <br> Öncesi Fiyatlar</th>
+                                <th>20 Nisan 2025 <br> Sonrası Fiyatlar</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Lisans ve Lisansüstü Öğrenciler</td>
+                                <td>5.000 TL</td>
+                                <td>6.000 TL</td>
+                            </tr>
+                            <tr>
+                                <td>Dernek Üyesi</td>
+                                <td>5.500 TL</td>
+                                <td>6.500 TL</td>
+                            </tr>
+                            <tr>
+                                <td>Dernek Üyesi Olmayan</td>
+                                <td>6.000 TL</td>
+                                <td>7.000 TL</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                        <!-- /table -->
+                    </div>
+                </center>
+
+                <div class="col-lg-12 col-md-12">
                     <ul class="custom no-margin">
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
+                        <li>Kayıt konaklama paket ücretlerinde KDV dahil değildir. KDV oranı %20'dir.</li>
+                        <li>Kongre genel alanları ve bilimsel toplantılara katılım için herkesin kayıt yaptırması zorunludur.</li>
+                        <li>Kayıt ücretine bilimsel aktivitelere katılım, yaka kartı, yaka kartı programı ve katılım sertifikası dahildir.</li>
+                        <li>Organizasyon muhtemel vergi, harç, rusum tutarlarındaki değişikliği fiyatlara yansıtma hakkını saklı tutar.</li>
 
                     </ul>
 				</div>
-                <center>
-                    <div class="col-md-8" style="float: none;">
-                        <!-- Tables -->
-                        <table class="table table-bordered table-striped table-hover ohd-table">
-                            <thead>
-                            <tr>
-                                <th>Kayıt Tipi</th>
-                                <th>Kayıt Ücreti</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <td>Uzman Kayıt</td>
-                                <td>₺ 21.000,00</td>
-                            </tr>
-                            <tr>
-                                <td>Firma Temsilcisi</td>
-                                <td>₺ 15.000,00</td>
-                            </tr>
-                            <tr>
-                                <td>Kurs Kayıt</td>
-                                <td>₺ 5.000,00</td>
-                            </tr>
-                            <tr>
-                                <td>Sponsorluğu Olmayan Firma Temsilcisi</td>
-                                <td>₺ 25.000,00</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <!-- /table -->
-                    </div>
-                </center>
 				<div class="col-md-12 col-md-12 res-margin">
                     <h3 class="text-center mt-5">KONAKLAMA</h3>
+                    <p><b>Konaklama talepleriniz için lütfen iletişime geçiniz.</b></p>
+                    <p><b>GSM:</b><a href="tel:+905343366908">+90 534 336 69 08</a></p>
+                    <p><b>Mail:</b><a href="mailto:salih.gonca@bosphorusmice.net "> salih.gonca@bosphorusmice.net </a></p>
                 </div>
-                <div class="col-lg-12 col-md-12">
-                    <h4 class="sub-header">Kongre Konaklama</h4>
-                    <ul class="custom no-margin">
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-                        <li>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas.</li>
-
-                    </ul>
-                </div>
-				<center>
-                    <div class="col-md-6" style="float: none">
+				<div class="d-flex w-100 justify-content-center">
+                    <div class="col-md-8 mt-5" style="float: none">
                         <!-- Tables -->
-                        <table class="table table-bordered table-striped table-hover ohd-table">
+                        <table class="table table-bordered table-striped table-hover ohd-table-billing">
                             <thead>
                             <tr>
-                                <th>Konaklama Tipi</th>
-                                <th>Konaklama Ücreti</th>
+                                <th colspan="3" class="text-center">HESAP BİLGİLERİ</th>
                             </tr>
                             </thead>
                             <tbody>
                             <tr>
-                                <td>Tek Kişilik Konaklama</td>
-                                <td>€ 1.100,00</td>
+                                <td>Alıcı</td>
+                                <td>Bosphorus Kongre Organizasyon Etkinlik ve Danışmanlık Hiz.</td>
                             </tr>
                             <tr>
-                                <td>İki Kişilik Oda (Kişi Başı)</td>
-                                <td>€ 850,00</td>
+                                <td>Banka</td>
+                                <td>Garanti BBVA</td>
+                            </tr>
+                            <tr>
+                                <td>Şube</td>
+                                <td>Koşuyolu (1016)</td>
+                            </tr>
+                            <tr>
+                                <td>IBAN (TL)</td>
+                                <td>TR04 0006 2001 0160 0006 2962 04</td>
                             </tr>
                             </tbody>
                         </table>
                         <!-- /table -->
                     </div>
-                </center>
+                </div>
 
 			</section><!-- /section  -->
 		</div><!--/page-section-->
