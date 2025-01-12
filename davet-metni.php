4b25127963c26be54137783d6da820a0b2600fe4
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-01-13 01:36:15 +0300

    Davet metni eklendi.

--- davet-metni.php
+++ davet-metni.php
@@ -1,41 +1,60 @@
 <div class="col-md-12 col-md-12 res-margin" style="padding-bottom: 60px;">
-	<p><b>Lorem İpsum,</b></p>
-    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Aliquet ante pharetra himenaeos platea porta a egestas. Pellentesque ac scelerisque risus rhoncus condimentum. Porta purus mattis aptent bibendum nam consectetur quisque. Luctus sed nisl et interdum pellentesque bibendum sed. Phasellus morbi vitae molestie porttitor nibh neque. Senectus ullamcorper mauris ridiculus class augue netus. Rutrum curae risus dui sit dignissim netus sed lacus.</p>
-    <p>Molestie duis malesuada luctus, mi dignissim per. Potenti morbi ligula fringilla luctus sagittis dui. Dui laoreet porttitor aliquam nostra in eleifend magnis. Facilisi praesent hac, curae pharetra sapien semper. Habitasse per erat dapibus praesent tempor eget dis auctor. Senectus maximus justo varius; urna dictum senectus laoreet aliquam. Mi iaculis ex rhoncus; facilisi nullam suspendisse.</p>
-     <p><b>Lorem İpsum,<br>
-    Onkoloji Hemşireleri Derneği</b></p>
+    <p><b>Değerli Meslektaşlarımız,</b></p>
+    <p>
+        Onkoloji Hemşireliği Derneği olarak, 35 yıllık birikim ve deneyimimizle, meslektaşlarımızın değerli katkıları ve
+        yoğun talepleri doğrultusunda ilk uluslararası kongremizi düzenlemenin onurunu ve gururunu yaşıyoruz .
+        <b>I. Uluslararası Onkoloji Hemşireliği Derneği Kongresi</b>’ni, 21-23 Mayıs 2025 tarihleri arasında Gazi
+        Üniversitesi
+        Hemşirelik Fakültesi ile iş birliği içinde, Ankara’da gerçekleştireceğiz. Sizleri bu tarihi etkinliğe davet
+        etmekten büyük mutluluk duyuyoruz.
+    </p>
+    <p>
+        Onkoloji hemşireleri birey, aile ve toplumun onkoloji alanında farkındalığını artırarak ve kaliteli bakım
+        hizmeti
+        sunarak toplum sağlığının korunması ve yükseltilmesinde önemli bir rol oynar. Bu bağlamda kongrenin ana teması
+        “Onkoloji Hemşireliğinde 35 Yıllık Miras: Geçmişten Geleceğe” olarak belirlenmiştir. Kongremiz, onkoloji
+        hemşireliğine yön veren ulusal ve uluslararası düzeyde saygın bilim insanlarının, uzmanların ve klinik
+        uygulayıcıların bir araya geleceği eşsiz bir platform olacaktır. Onkoloji hemşireliği uygulamasında Kanserden
+        Korunma ve Erken Tanı: Farkındalık ve Hayat Kurtaran Stratejiler; Kanser Bakımında Yenilikçi ve Sürdürülebilir
+        Hemşirelik Yaklaşımları; Teknolojik Gelişmelerin Onkoloji Hemşireliğine Yansımaları, Kanserle Yaşamak: Yeni Bir
+        Denge ve Onkoloji Hemşireliğinde Yaşanan Sorunlar ve Çözüm Önerileri gibi alanlarda bilgi ve deneyim
+        paylaşımının yapılacağı bu kongre, alanımızda geleceği şekillendiren tartışmalara ev sahipliği yapacaktır.
+    </p>
+    <p>
+        Bu tarihi etkinlikte sizlerin değerli katkıları, bilgi birikiminiz ve katılımınızla kongremizin daha da
+        zenginleşeceğine inanıyoruz. Hep birlikte onkoloji hemşireliği alanında yeni ufuklar açacağımız bu önemli
+        buluşmada sizleri aramızda görmekten büyük mutluluk duyacağız.
+    </p>
+    <p>Birlikte daha ileriye! br
+        Kongremizde görüşmek dileğiyle,
+    </p>
 </div>
-<!--<div class="w-100 row justify-content-md-center" style="padding-bottom: 50px;margin-right: 0;">-->
-<!--	<div class="col-md-2"></div>-->
-<!--	<div class="col-md-4">-->
-<!--		<div class="our-team" style="padding-bottom: 30px;">-->
-<!--			<div class="pic">-->
-<!--				<img alt="" class="img-responsive img-circle" src="doc/haluk-hoca2.jpg" style="max-width: 120px;-->
-<!--                                        border: 2px solid;-->
-<!--                                        height: auto;-->
-<!--                                        margin: 0 auto;-->
-<!--                                        border-radius: 20%;-->
-<!--                                        border-color: #fc8c76;">-->
-<!--				<div class="" style="text-align: center;">-->
-<!--					<h6 style="text-transform: inherit;">Prof. Dr. Haluk Çokuğraş</h6><span class=""><b>Çocuk Alerji ve Astım Akademisi Derneği Başkanı</b></span>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	</div>-->
-<!--	<div class="col-md-4">-->
-<!--		<div class="our-team" style="padding-bottom: 30px;">-->
-<!--			<div class="pic">-->
-<!--				<img alt="" class="img-responsive img-circle" src="doc/metin-aydogan.jpg" style="max-width: 120px;-->
-<!--                                        border: 2px solid;-->
-<!--                                        height: auto;-->
-<!--                                        margin: 0 auto;-->
-<!--                                        border-radius: 20%;-->
-<!--                                        border-color: #fc8c76;">-->
-<!--				<div class="" style="text-align: center;">-->
-<!--					<h6 style="text-transform: inherit;">Prof. Dr. Metin Aydoğan</h6><span class=""><b>Kongre Başkanı</b></span>-->
-<!--				</div>-->
-<!--			</div>-->
-<!--		</div>-->
-<!--	</div>-->
-<!---->
-<!--</div>-->
+<div class="w-100 row justify-content-md-center" style="padding-bottom: 50px;margin-right: 0;">
+	<div class="col-md-2"></div>
+	<div class="col-md-4">
+		<div class="our-team" style="padding-bottom: 30px;">
+			<div class="pic">
+				<div class="" style="text-align: center;">
+                    <div style="width: 120px;border: 2px solid;height: auto;margin: 0 auto;border-radius: 20%;border-color: #fc8c76;"></div>
+					<h6 style="text-transform: inherit;">Prof. Dr. Sultan Ayaz	Alkaya</h6>
+                    <span class="">
+                        <b>Kongre Başkanı</b>
+                    </span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="our-team" style="padding-bottom: 30px;">
+			<div class="pic">
+				<div class="" style="text-align: center;">
+                    <div style="width: 120px;border: 2px solid;height: auto;margin: 0 auto;border-radius: 20%;border-color: #fc8c76;"></div>
+                    <h6 style="text-transform: inherit;">Prof. Dr. Perihan Güner</h6>
+                    <span class="">
+                        <b>Kongre Başkanı</b>
+                    </span>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
