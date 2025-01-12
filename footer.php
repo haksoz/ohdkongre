da8a55df3cb1d0cf5c1b6b7a306ce73fbf0a84bc
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-01-13 01:36:33 +0300

    Footer tasarımı ve içeriği revize edildi.

--- footer.php
+++ footer.php
@@ -1,44 +1,47 @@
 <!-- Footer starts -->
 <footer>
     <div class="container">
-    <div class="col-md-2 text-center">
-            <!-- Footer logo -->
-            <img alt="" class="res-margin center-block img-responsive" src="/doc/cbmd-logo.png"> <!--Social icons -->
-            
-        </div><!-- Credits-->
-        <div class="col-md-4 res-margin">
-            <ul class="list-unstyled footer-icons">
-                <li><i class="fa fa-map-marker"></i><b>Adres:</b>Ankara/Türkiye
-                </li>
-<!--                <li><i class="fa fa-phone"></i><b>Tel:</b><a href="tel:+902125340452"> +90 212 534 04 52</a></li>-->
-<!--                <li><i class="fa fa-phone"></i><b>Tel:</b><a href="+905372688548"> +90 537 268 85 48</a></li>-->
-                <li>
-                    <i class="fa fa-envelope"></i><b>E-Mail:</b><a href="mailto:info@onkohem.org.tr"> info@onkohem.org.tr</a>
-                </li>
-            </ul>
-        </div><!-- /.col-md-4 -->
-        <div class="col-md-2 text-center">
-            <!-- Footer logo -->
-            <img alt="" class="res-margin center-block img-responsive" src="/doc/solo.png"> <!--Social icons -->
-        </div><!-- Credits-->
-        <div class="col-md-4 res-margin">
-            <ul class="list-unstyled footer-icons">
-                <li><i class="fa fa-map-marker"></i><b>Adres:</b> Kozyatağı Mah. Çardak Sok. Şaşmaz Sitesi B2 Blok
-                    No:4 D:10 Kadıköy/İstanbul</li>
-                <li><i class="fa fa-phone"></i><b>Tel:</b><a href="tel:+902167661870"> +90 (216) 766 18 70</a></li>
-                <li>
-                    <i class="fa fa-envelope"></i><b>E-Mail:</b><a href="mailto:salih@momentosevent.com"> salih@momentosevent.com</a>
-                </li>
-            </ul>
-            
-        </div><!-- /.col-md-4 -->
-        
-        <div class="credits col-md-12 text-center">
-            <hr>
-            <span class="text-white">Copyright © 2024 -</span> <a href="https://ko.com.tr" target="_blank">Kapital Online</a> <!-- Go To Top Link -->
-            <div class="page-scroll hidden-sm hidden-xs">
-                <a class="back-to-top" href="#page-top"><i class="fa fa-angle-up"></i></a>
-            </div>
-        </div><!-- /credits -->
-    </div><!-- /.container -->
+        <div class="d-flex justify-content-around flex-wrap">
+            <div class="col-md-4 res-margin">
+                <ul class="list-unstyled footer-icons">
+                    <img alt="" class="res-margin center-block footer-logo"
+                         src="/doc/onkoloji-hemsireligi-dernegi-logo.png">
+                    <li>
+                        <i class="fa fa-map-marker"></i><b>Adres:</b>
+                        Ankara/Türkiye
+                    </li>
+                    <li>
+                        <i class="fa fa-envelope"></i><b>E-Mail:</b>
+                        <a href="mailto:info@onkohem.org.tr"> info@onkohem.org.tr</a>
+                    </li>
+                </ul>
+            </div><!-- /.col-md-4 -->
+            <div class="col-md-4 res-margin">
+                <ul class="list-unstyled footer-icons">
+                    <img alt="" class="res-margin center-block footer-logo" src="/doc/bosphorusmice.jpeg">
+                    <li>
+                        <i class="fa fa-map-marker"></i><b>Adres:</b>
+                        Koşuyolu Mah. İsmailpaşa Sok, No:42/1, Kadıköy, İstanbul
+                    </li>
+                    <li>
+                        <i class="fa fa-phone"></i><b>Tel:</b>
+                        <a href="tel:+905343366908"> +90 534 336 69 08</a>
+                    </li>
+                    <li>
+                        <i class="fa fa-envelope"></i><b>E-Mail:</b>
+                        <a href="mailto:salih.gonca@bosphorusmice.net"> salih.gonca@bosphorusmice.net</a>
+                    </li>
+                </ul>
+
+            </div><!-- /.col-md-4 -->
+        </div><!-- /.container -->
+    </div>
+    <div class="credits text-center">
+        <hr>
+        <span class="text-white">Copyright © 2024 -</span> <a href="https://ko.com.tr" target="_blank">Kapital
+            Online</a> <!-- Go To Top Link -->
+        <div class="page-scroll hidden-sm hidden-xs">
+            <a class="back-to-top" href="#page-top"><i class="fa fa-angle-up"></i></a>
+        </div>
+    </div><!-- /credits -->
 </footer><!-- /footer ends -->
