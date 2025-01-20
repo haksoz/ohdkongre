b23bc8bc9c0c1b5f6fd3d95550e173e40aa0d744
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-01-20 13:50:13 +0300

    bildiri gönderim sayfası hazırlandı.

--- bildiri-gonderimi.php
+++ bildiri-gonderimi.php
@@ -37,12 +37,167 @@
                     <h2 class="mt-5">BİLDİRİ GÖNDERİMİ</h2><!-- divider -->
                     <div class="hr"></div>
                 </div>
-                <div class="row justify-content-center" style="padding-top: 10%;padding-bottom: 10%;">
-                    <div class="text-center">
-                        <h4 class="text-center">Yapım Aşamasında</h4>
-                    </div>
+                <div class="d-flex flex-column justify-content-center align-items-center" style="padding-bottom:30px;">
+                    <p class="text-center"><b>Bildiri Özet Son Gönderim Tarihi: <span class="text-red">15 Nisan 2025</span></b></p>
+                    <a href="#" target="_blank" class="abstract-button">
+                        <img src="/doc/abs.png">
+                    </a>
                 </div>
-			</section><!-- /section  -->
+                <div>
+                    <h4>Bildiri Gönderim Kuralları</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li> Bildiriler yalnızca kongre web sitesi üzerinden <b>Online Bildiri Sistemi</b> aracılığıyla gönderilebilecektir.
+                            <br>
+                           &nbsp;&nbsp;&nbsp;&nbsp; E-posta, faks veya posta yoluyla gönderilen bildiriler kabul edilmeyecektir.</li>
+                        <li> Bildiri göndermeden önce, online sisteme kayıt yaptırılması gerekmektedir. Daha önce bir üyelik hesabınız yoksa, sistem üzerinden yeni bir hesap oluşturabilirsiniz.</li>
+                        <li> Her katılımcı <b>en fazla iki bildiri</b> gönderebilir. Daha fazla sayıda bildiri gönderilmesi durumunda her bir bildiri için bildiri ücreti alınacaktır.</li>
+                        <li> Bildiriler özet bildiri ya da tam metin bildiri olarak kabul edilecektir.</li>
+                        <li> Bildiriler, sistem üzerinden gönderildiği şekilde yayınlanacağı için yazım hatalarına dikkat edilmelidir. Tüm yazım hatalarından yazarlar sorumludur.</li>
+                        <li> Bildirinin kabul edilmesi durumunda, bildiriyi sunacak yazarın kongre kaydını tamamlaması zorunludur. Aksi takdirde, bildiri kongre özet kitabında yayınlanmayacaktır.</li>
+                        <li> Bildiri gönderimi sırasında, sistem üzerinden <b>davet mektubu</b> otomatik olarak oluşturulacaktır.</li>
+
+                    </ul>
+                </div>
+                <div>
+                    <h4>Bildiri Kategorileri</h4>
+                    <p>Kongreye aşağıdaki kategorilerde sözlü ve poster bildiri gönderimi yapılabilir:</p>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li> Kanserden Korunma ve Erken Tanı: Farkındalık ve Hayat Kurtaran Stratejiler</li>
+                        <li> 21.Yüzyılda Kanser Bakımında Yenilikçi ve Sürdürülebilir Hemşirelik Yaklaşımları</li>
+                        <li> Güncel Teknolojik Gelişimlerin Onkoloji Hemşireliğine Yansımaları</li>
+                        <li> Kanserle Yaşamak: Yeni Bir Denge</li>
+                        <li> Onkoloji Hemşireliğinde Yaşanan Sorunlar ve Çözüm Önerileri</li>
+                        <li> Onkoloji Hemşireliği Araştırmalarında Yeni Ufuklar</li>
+
+                    </ul>
+                </div>
+                <div>
+                    <h4>Bildiri Özet Gönderim Kuralları</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li> Bildiri özetleri, <b>Türkçe ve İngilizce</b> olarak hazırlanmalı ve sisteme yüklenmelidir.</li>
+                        <li> İngilizce ve Türkçe özetin her biri (başlık, yazar adı, soyadı, kurum bilgileri ve anahtar kelimeler hariç) <b>400 kelimeyi geçmemelidir.</b></li>
+                        <li> Özetler, <b>Times New Roman </b>yazı tipinde,<b> 11 punto</b> büyüklüğünde ve <b>1.5 satır aralığı</b> ile hazırlanmalıdır.</li>
+                        <li> Özet başlığı:
+                            <ul>
+                                <li>Çalışmanın içeriğini yansıtmalı, kısa ve öz olmalı, kısaltma içermemelidir.</li>
+                                <li>Türkçe başlıklarda yalnızca ilk harf büyük yazılmalı, İngilizce başlıklarda ise "on, of" gibi edatlar küçük harfle yazılmalıdır.</li>
+                            </ul>
+                        </li>
+                        <li> Yazar isimlerinde akademik unvan kullanılmamalıdır. İsimler yalnızca ilk harf büyük olacak şekilde yazılmalıdır.</li>
+                        <li> Yazarların çalıştıkları kurumların isim ve adresleri (üniversite adı, fakültesi/bölümü, ili ve ülkesi) mutlaka belirtilmelidir.</li>
+                        <li> Özet içinde kısaltma kullanılması gerekiyorsa, kısaltmanın ilk geçtiği yerde parantez içinde açıklaması yapılmalıdır.</li>
+                        <li> Bildiri özetleri şu bölümleri içermelidir:
+                            <ul>
+                                <li><b>Orijinal Araştırma:</b> Giriş ve Amaç, Yöntem, Bulgular, Sonuç</li>
+                                <li><b>Olgu Sunumu:</b> Giriş ve Amaç, Olgu(lar), Sonuç</li>
+                                <li><b>Derleme:</b> Derlemenin amacı ve içeriği düz metin olarak belirtilmelidir.</li>
+                            </ul>
+                        </li>
+                        * Kongrede olgu sunumu ve derlemeler sadece poster bildiri olarak değerlendirilmeye alınacaktır.
+                        <li> Anahtar kelimeler:
+                            <ul>
+                                <li>En az <b>3</b>, en fazla <b>5 kelime</b> olmalıdır.</li>
+                                <li>Kelimeler arasında “,” (virgül) kullanılmalı ve alfabetik sırayla yazılmalıdır.</li>
+                                <li>Türkçe anahtar kelimeler Türkiye Bilim Terimleri (TBT)’ne uygun olarak verilmelidir.
+                                    İngilizce anahtar kelimeler
+                                    <a href="www.nlm.nih.gov/mesh/MBrowser.html" target="_blank">Medical Subject Headings (MeSH)</a> 'e uygun olarak verilmelidir.
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Tam Metin Bildiri Hazırlama Kuralları</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li>Tam metin gönderimi zorunlu değildir. Kongreye yalnızca özet bildiri ile katılım sağlanabilir.</li>
+                        <li>Tam metinler, özet bildiri sonuçları açıklandıktan sonra sisteme yüklenebilir. Özetleri kabul edilmeyen katılımcılar tam metin gönderemez.</li>
+                        <li>Tam metinler:
+                            <ul>
+                                <li><b>Türkçe veya İngilizce</b> olarak hazırlanabilir.</li>
+                                <li><b>1500-2500</b> kelime arasında olmalıdır (başlık, yazar bilgileri ve kaynakça hariç).
+                                </li>
+                                <li><b>Times New Roman, 11 punto, 1.5 satır aralığı</b> ile yazılmalıdır.</li>
+                                <li>Bütün tablo ve şekil/grafiklerin başlıklarının yazılması ve metinde kullanılış
+                                    sırasına göre yerleştirilmesi gerekir. Şekiller dijital formatlı, JPEG kayıtlı ve
+                                    300 dpi çözünürlüğünde olmalıdır.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Tam metin bölümleri:
+                            <ul>
+                                <li><b>Orijinal Araştırma:</b> Özet (Türkçe ve İngilizce), Giriş ve Amaç, Yöntem, Bulgular,
+                                    Tartışma, Sonuç, Kaynaklar
+                                </li>
+                                <li><b>Olgu Sunumu:</b> Özet (Türkçe ve İngilizce), Giriş ve Amaç, Olgu(lar), Tartışma, Sonuç,
+                                    Kaynaklar
+                                </li>
+                                <li><b>Derleme:</b> Özet (Türkçe ve İngilizce), Giriş ve Amaç, varsa alt başlıklar, Sonuç,
+                                    Kaynaklar
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Anahtar kelimeler:
+                            <ul>
+                                <li>En az <b>3</b>, en fazla <b>5 kelime</b> olmalıdır.</li>
+                                <li>Kelimeler arasında “,” (virgül) kullanılmalı ve alfabetik sırayla yazılmalıdır.</li>
+                                <li>Türkçe anahtar kelimeler Türkiye Bilim Terimleri (TBT)’ne uygun olarak verilmelidir.
+                                    İngilizce anahtar kelimeler
+                                    <a href="www.nlm.nih.gov/mesh/MBrowser.html" target="_blank">Medical Subject Headings (MeSH)</a> 'e uygun olarak verilmelidir.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Kaynaklar, APA 7 stiline uygun olarak yazılmalıdır.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Bildirilerin Değerlendirilmesi</h4>
+                    <!-- Custom List -->
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li>Bildiriler, yazarların adı, soyadı ve kurum bilgileri gizli tutularak bağımsız hakemler tarafından değerlendirilecektir.</li>
+                        <li>Hakem raporlarına göre bildirilerin kabulü, reddi veya düzeltme istenmesi mümkündür.</li>
+                        <li>Bildiri kabul veya ret sonuçları, başvuru yapan yazara elektronik posta yoluyla bildirilecektir.</li>
+                    </ul>
+                </div>
+                <div>
+                    <h4>Bildirilerin Sunulması</h4>
+                    <p style="font-weight: bold;">Sözlü Bildiri</p>
+                    <ul class="custom no-margin" style="font-weight:normal;">
+
+                        <li>Sözlü bildiriler Türkçe veya İngilizce olarak sunulabilir.</li>
+                        <li>Sunum süresi, tartışma dahil toplam <b>10 dakika</b> ile sınırlıdır.</li>
+                        <li>Sunumlar, kongre programında belirtilen gün, saat ve salonda yapılmalıdır. Belirtilen zamanda sunum yapmayanlar kongreye katılmamış sayılacaktır.</li>
+                        <li>Başkasının adına bildiri sunulamaz. Sunum yapacak kişi mutlaka yazar veya ortak yazar olmalıdır.</li>
+
+                    </ul>
+                    <p style="font-weight: bold;">Poster Bildiri</p>
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li>Posterler, <b>70cm x 90cm</b> boyutunda ve e-poster olarak hazırlanmalıdır.</li>
+                        <li>Posterler sisteme pdf dosyası olarak yüklenecektir.</li>
+                        <li>Posterlerde şu bölümler yer almalıdır:
+                            <ul>
+                                <li><b>Orijinal Araştırma:</b> Giriş ve Amaç, Yöntem, Bulgular, Tartışma, Sonuç, Kaynaklar</li>
+                                <li><b>Olgu Sunumu:</b> Giriş ve Amaç, Olgu(lar), Tartışma, Sonuç, Kaynaklar</li>
+                                <li><b>Derleme:</b> Giriş ve Amaç, varsa alt başlıklar, Sonuç, Kaynaklar</li>
+                            </ul>
+                        </li>
+                        <li>Posterler, kongre süresince belirlenen alanda sergilenmelidir.</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4>Bildirilerin Yayınlanması</h4>
+                    <ul class="custom no-margin" style="font-weight:normal;">
+                        <li>Kongrede sunulan tüm bildiriler kongre özet kitabında elektronik olarak yayınlanacaktır.</li>
+                        <li>Sunumu yapılmayan bildiriler, özet kitabında yer almayacaktır.</li>
+                        <li>Bildirilerle ilgili tüm sorumluluk (etik onay, kaynakların doğru yazımı, dil bilgisi kuralları vb.) yazarlara aittir.</li>
+                    </ul>
+                </div>
+
+            </section><!-- /section  -->
 		</div><!--/page-section-->
 
 		<?php include("footer.php") ?>
