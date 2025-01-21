bff7a1edbc5c7a08a46448bd97e724324f2d9abb
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-01-21 16:19:32 +0300

    kurullar iki sayfaya ayrıldı.

---
+++ duzenleme-kurulu.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="TR">
+<?php $page = 'kurullar'; ?>
+<head>
+    <title>1. Uluslararası Onkoloji Hemşireliği Derneği Kongresi | Düzenleme Kurulu</title>
+    <?php include("head.php") ?>
+</head>
+<body data-spy="scroll" data-target=".navbar-custom" id="page-top">
+<!-- Preloader  -->
+<div id="preloader">
+    <div class="spinner">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+</div><!-- /Preloader ends -->
+<div id="page-width">
+    <?php include("header.php") ?>
+    <!-- Blog Page -->
+    <!-- Blog Single Page  -->
+    <div id="page-section">
+        <section id="blog">
+            <div class="container-fluid">
+                <div class="jumbotron"></div>
+                <div class="jumbo-heading">
+                    <div class="container">
+                        <img src="/doc/slider-banner2.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="section-heading">
+                    <h2 class="mt-5">Kurullar</h2><!-- divider -->
+                    <div class="hr"></div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="text-center col-md-12">
+                        <h4>ONURSAL BAŞKANLAR</h4>
+                        <p>
+                            Prof. Dr. Uğur ÜNAL <br>
+                            Prof. Dr. Nurgün PLATİN <br>
+                            Prof. Dr. Gülşen TERAKYE
+                        </p>
+
+                        <h4>KONGRE BAŞKANLARI</h4>
+                        <p>
+                            Prof. Dr. Perihan GÜNER <br>
+                            Prof. Dr. Sultan AYAZ ALKAYA
+                        </p>
+
+                        <h4>ONKOLOJİ HEMŞİRELİĞİ DERNEĞİ GEÇMİŞ BAŞKANLARI</h4>
+                        <p>
+                            Prof. Dr. Leman BİROL <br>
+                            Prof. Dr. Nurgün PLATİN <br>
+                            Prof. Dr. Lale TAŞKIN <br>
+                            Prof. Dr. Gülşen TERAKYE <br>
+                            Prof. Dr. Yurdagül ERDEM <br>
+                            Prof. Dr. Sultan KAV <br>
+                            Dr. Öğr. Üyesi Fatma GÜNDOGDU <br>
+                            Doç. Dr. Sevcan ATAY <br>
+                            Prof. Dr. Sevinç KUTLUTÜRKAN <br>
+                            Uzm. Hem. Figen BAY <br>
+
+                            <span class="text-red">
+                            *Başkanlık dönemine göre sıralanmıştır.
+                        </span>
+                        </p>
+
+                        <h4>KONGRE SEKRETERYASI</h4>
+                        <p>
+                            Dr. Öğr. Üyesi Elif SÖZERİ ÖZTÜRK <br>
+                            Dr. Öğr. Üyesi Fatma GÜNDOGDU <br>
+                            Dr. Öğr. Üyesi Özlem CANBOLAT <br>
+                            Hem. Ferhan ÇETİN ŞEREF <br>
+                        </p>
+
+                        <h4>KONGRE DÜZENLEME KURULU</h4>
+                        <p>
+                            Prof. Dr. Ayşe OKANLI <br>
+                            Prof. Dr. Gülbeyaz CAN <br>
+                            Prof. Dr. Gülten GÜVENÇ <br>
+                            Prof. Dr. Özlem UĞUR <br>
+                            Prof. Dr. Sevil GÜLER <br>
+                            Prof. Dr. Sevinç KUTLUTÜRKAN <br>
+                            Doç. Dr. Betül TOSUN <br>
+                            Doç. Dr. Dilek YILDIRIM <br>
+                            Doç. Dr. Fatma ARIKAN <br>
+                            Doç. Dr. Figen ŞENGÜN İNAN <br>
+                            Doç. Dr. Vildan KOCATEPE <br>
+                            Dr. Öğr. Üyesi Miray AKSU <br>
+                            Dr. Öğr. Üyesi Nevra KALKAN <br>
+                            Dr. Öğr. Üyesi Yasemin KARACAN <br>
+                            Arş. Gör. Hazal ÖZDEMİR KOYU <br>
+                            Arş. Gör. Neslihan KÖSE KABAKÇIOĞLU <br>
+                            Uzm. Hem. Ayşin KAYIŞ <br>
+                            Uzm. Hem. Canan PÖRÜCÜ <br>
+                            Uzm. Hem. Figen BAY <br>
+                            Hem. Hale SÜNBÜL <br>
+                            Hem. Pınar YILMAZ <br>
+                            Hem. Şerife KARACA <br>
+                            <span class="text-red">*İsimler alfabetik sıraya göre düzenlenmiştir.</span>
+                        </p>
+
+                    </div><!-- /col-md-12-->
+                </div>
+            </div>
+        </section><!-- /section  -->
+    </div><!--/page-section-->
+
+    <?php include("footer.php") ?>
+</div><!-- /page-width -->
+
+<?php include("script.php") ?>
+</body>
+</html>
