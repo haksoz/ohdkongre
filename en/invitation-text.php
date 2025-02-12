f4686e40fc1a2ae449ce018359096114360f1d6b
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-02-12 17:30:48 +0300

    metin revizesi

--- en/invitation-text.php
+++ en/invitation-text.php
@@ -1,20 +1,30 @@
 <div class="col-md-12 col-md-12 res-margin" style="padding-bottom: 60px;">
     <p><b>Dear Colleagues,</b></p>
     <p>
-        As the Oncology Nursing Association, with 35 years of accumulated knowledge and experience, we are honored and proud to organize our first international congress based on the valuable contributions and high demand from our colleagues.
-        We will hold the <b>1st International Oncology Nursing Association Congress</b> on May 21–23, 2025, in collaboration with the Gazi University Faculty of Nursing in Ankara. We are delighted to invite you to this historic event.
+        As Oncology Nursing Association, we are proud and honored to organize our first international congress in
+        accordance with the valuable contributions and intense demands of our colleagues with our 35 years of knowledge
+        and experience. In cooperation with Gazi University Faculty of Nursing, we will hold the <b>1st International
+            Congress of Oncology Nursing Association in Ankara between 21-23 May 2025</b>. We look forward to inviting
+        you to
+        this historic event.
     </p>
     <p>
-        Oncology nurses play a vital role in enhancing public health by raising awareness in oncology for individuals, families, and society, and by providing quality care services. In this context, the main theme of the congress has been determined as
-        "35 Years of Legacy in Oncology Nursing: From Past to Future." Our congress will be a unique platform where respected national and international scientists, experts, and clinical practitioners shaping the field of oncology nursing come together.
-        Topics such as Prevention and Early Detection of Cancer: Awareness and Life-Saving Strategies; Innovative and Sustainable Nursing Approaches in Cancer Care; The Impact of Technological Developments on Oncology Nursing; Living with Cancer: A New Balance;
-        and Challenges and Solutions in Oncology Nursing will be addressed, facilitating the sharing of knowledge and experiences. This congress will host discussions that shape the future of our field.
+        Oncology nurses are essential in protecting and promoting public health by raising awareness of oncology among
+        individuals, families, and society and providing quality nursing care. In this context, the central theme of the
+        congress is "35 Years of Legacy in Oncology Nursing: From the Past to the Future". Our congress will be a unique
+        platform bringing together nationally and internationally renowned scientists, experts, and clinical
+        practitioners shaping oncology nursing. Cancer Prevention and Early Diagnosis in Oncology Nursing Practice:
+        Awareness and Lifesaving Strategies; Innovative and Sustainable Nursing Approaches in Cancer Care; Reflections
+        of Technological Developments in Oncology Nursing; Living with Cancer: A New Balance, and Problems and Solutions
+        in Oncology Nursing, this congress will host discussions that will shape the future of our field.
     </p>
     <p>
-        We believe that your valuable contributions, expertise, and participation will further enrich our congress. We would be delighted to see you among us at this important meeting where we will open new horizons together in oncology nursing.
+        Our congress will be even more decadent with your valuable contributions, knowledge, and participation in this
+        historic event. We look forward to seeing you at this pivotal meeting, where we will open new horizons in the
+        field of oncology nursing.
     </p>
     <p>Forward together! <br>
-        Looking forward to seeing you at our congress,
+        Hope to see you at our congress,
     </p>
 </div>
 <div class="w-100 row justify-content-md-center" style="padding-bottom: 50px;margin-right: 0;">
@@ -26,7 +36,7 @@
                     <div style="width: 120px;border: 2px solid;height: auto;margin: 0 auto;border-radius: 20%;border-color: #fc8c76;"></div>
                     <h6 style="text-transform: inherit;">Prof. Dr. Sultan Ayaz Alkaya</h6>
                     <span class="">
-                        <b>Congress Chair</b>
+                        <b>Congress President </b>
                     </span>
                 </div>
             </div>
@@ -39,7 +49,7 @@
                     <div style="width: 120px;border: 2px solid;height: auto;margin: 0 auto;border-radius: 20%;border-color: #fc8c76;"></div>
                     <h6 style="text-transform: inherit;">Prof. Dr. Perihan Güner</h6>
                     <span class="">
-                        <b>Congress Chair</b>
+                        <b>Congress President </b>
                     </span>
                 </div>
             </div>
