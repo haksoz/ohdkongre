26d84986547bc3542c9f6b427554f0e0269012be
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-02-13 10:28:22 +0300

    versiyonlama

--- head.php
+++ head.php
@@ -11,20 +11,20 @@
       <script src="js/respond.js"></script>
       <![endif]-->
 	<!-- Bootstrap Core CSS -->
-	<link href="css\bootstrap.css?v=656345" rel="stylesheet" type="text/css"><!-- Icon fonts -->
-	<link href="fonts\font-awesome\css\font-awesome.min.css?v=656345" rel="stylesheet" type="text/css">
-	<link href="fonts\glyphicons\bootstrap-glyphicons.css?v=656345" rel="stylesheet" type="text/css">
-	<link href="fonts\flaticons\flaticon.css?v=656345" rel="stylesheet" type="text/css"><!-- Google fonts -->
+	<link href="css\bootstrap.css?v=12345t42" rel="stylesheet" type="text/css"><!-- Icon fonts -->
+	<link href="fonts\font-awesome\css\font-awesome.min.css?v=12345t42" rel="stylesheet" type="text/css">
+	<link href="fonts\glyphicons\bootstrap-glyphicons.css?v=12345t42" rel="stylesheet" type="text/css">
+	<link href="fonts\flaticons\flaticon.css?v=12345t42" rel="stylesheet" type="text/css"><!-- Google fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Baloo|Lato:400,700,900" rel="stylesheet"><!-- Style CSS -->
 	
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-	<link href="css\style.css?v=656345" rel="stylesheet"><!-- Plugins CSS -->
-	<link href="css\plugins.css?v=656345" rel="stylesheet"><!-- Color Style CSS -->
-	<link href="styles\pinkandblue.css?v=656345" rel="stylesheet"><!-- LayerSlider stylesheet -->
-	<link href="layerslider\css\layerslider.css?v=656345" rel="stylesheet"><!-- Favicons-->
+	<link href="css\style.css?v=12345t42" rel="stylesheet"><!-- Plugins CSS -->
+	<link href="css\plugins.css?v=12345t42" rel="stylesheet"><!-- Color Style CSS -->
+	<link href="styles\pinkandblue.css?v=12345t42" rel="stylesheet"><!-- LayerSlider stylesheet -->
+	<link href="layerslider\css\layerslider.css?v=12345t42" rel="stylesheet"><!-- Favicons-->
 	<link href="/apple-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
 	<link href="/apple-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
 	<link href="doc/favicon.png" rel="shortcut icon" type="image/x-icon"><!-- Switcher Only -->
