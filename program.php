60af316f78fd2ac3af7775764886408b27eac696
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-04-10 15:42:00 +0300

    Ayşin Kayış çıkarıldı.

--- program.php
+++ program.php
@@ -499,7 +499,7 @@
                                                 <td class="panel-baslik"></td>
                                                 <td class="panel-baslik">
                                                     <b>PANEL VII : KLİNİK UYGULAMADA FARK YARATANLAR: BAŞARI HİKAYELERİ </b> <br>
-                                                    <b>Panel Başkanları: </b> Fatma GÜNDOGDU, Ayşin KAYIŞ, Semra ERCİVAN
+                                                    <b>Panel Başkanları: </b> Fatma GÜNDOGDU, Semra ERCİVAN
                                                 </td>
                                             </tr>
                                             <tr>
