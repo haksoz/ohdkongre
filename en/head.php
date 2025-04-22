bc7d4e09affc0ffb154449f1e652b73de6092375
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-04-22 11:43:51 +0300

    versiyonlama

--- en/head.php
+++ en/head.php
@@ -11,19 +11,19 @@
       <script src="/js/respond.js"></script>
       <![endif]-->
 	<!-- Bootstrap Core CSS -->
-	<link href="/css/bootstrap.css?v=6234533" rel="stylesheet" type="text/css"><!-- Icon fonts -->
-	<link href="/fonts/font-awesome/css/font-awesome.min.css?v=6234533" rel="stylesheet" type="text/css">
-	<link href="/fonts/glyphicons/bootstrap-glyphicons.css?v=6234533" rel="stylesheet" type="text/css">
-	<link href="/fonts/flaticons/flaticon.css?v=6234533" rel="stylesheet" type="text/css"><!-- Google fonts -->
+	<link href="/css/bootstrap.css?v=623453323" rel="stylesheet" type="text/css"><!-- Icon fonts -->
+	<link href="/fonts/font-awesome/css/font-awesome.min.css?v=623453323" rel="stylesheet" type="text/css">
+	<link href="/fonts/glyphicons/bootstrap-glyphicons.css?v=623453323" rel="stylesheet" type="text/css">
+	<link href="/fonts/flaticons/flaticon.css?v=623453323" rel="stylesheet" type="text/css"><!-- Google fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Baloo|Lato:400,700,900" rel="stylesheet"><!-- Style CSS -->
 	
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-	<link href="/css/style.css?v=6234533" rel="stylesheet"><!-- Plugins CSS -->
-	<link href="/css/plugins.css?v=6234533" rel="stylesheet"><!-- Color Style CSS -->
-	<link href="/styles/pinkandblue.css?v=6234533" rel="stylesheet"><!-- LayerSlider stylesheet -->
-	<link href="/layerslider/css/layerslider.css?v=6234533" rel="stylesheet"><!-- Favicons-->
+	<link href="/css/style.css?v=623453323" rel="stylesheet"><!-- Plugins CSS -->
+	<link href="/css/plugins.css?v=623453323" rel="stylesheet"><!-- Color Style CSS -->
+	<link href="/styles/pinkandblue.css?v=623453323" rel="stylesheet"><!-- LayerSlider stylesheet -->
+	<link href="/layerslider/css/layerslider.css?v=623453323" rel="stylesheet"><!-- Favicons-->
 	<link href="/doc/favicon.png" rel="shortcut icon" type="image/x-icon"><!-- Switcher Only -->
 <!--	<link href="switcher\css\switcher.css" id="switcher-css" media="all" rel="stylesheet" type="text/css">
