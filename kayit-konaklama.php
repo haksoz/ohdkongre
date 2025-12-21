c124639354baf478169846a10518eb741b7c662c
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2025-12-21 14:01:22 +0300

    konaklama otelleri eklendi.

--- kayit-konaklama.php
+++ kayit-konaklama.php
@@ -134,11 +134,53 @@
                             <h3> Konaklama</h3>
                         </div>
                     </div>
-                    <div class="col-lg-12 m-auto">
+                    <!-- <div class="col-lg-12 m-auto">
                         <div class="heading2 space-margin60">
                             <ul>
                                 <li><i class="fa-solid fa-arrow-right"></i>Lütfen konaklama taleplerinizi için <a href="mailto:ohdkongre@bosphorusmice.net">ohdkongre@bosphorusmice.net</a> adresine iletiniz.</li>
                             </ul>
+                        </div>
+                    </div> -->
+                    
+                    <!-- Otel Bilgileri -->
+                    <div class="col-lg-12 m-auto">
+                        <div class="space-margin60">
+                            <!-- Clarion Hotel Golden Horn -->
+                            <div class="mb-5">
+                                <h4 class="mb-3" style="color: #604c59;">Clarion Hotel Golden Horn</h4>
+                                <p>
+                                    Clarion Hotel Golden Horn sizi bekliyor… İstanbul'un canlı hayatının titreşimlerini hissedip, bu büyüleyici şehri yepyeni otelimiz ile keşfetmeye ne dersiniz? Tüm dünyada 6,400'den fazla oteli ile başarılı otel zincirlerinden biri olan Choice Hotels International® ailesinin yeni üyesi Clarion Hotel Golden Horn modern tasarımı, Beyoğlu bölgesindeki merkezi konumu ile Haliç'in kıyısında yer almaktadır. Her çeşit ulaşım alternatiflerine olan yakınlığı ve önünde uzanan Altın Boynuz' un sunduğu huzur; otelimizin konumunu eşsiz yapmaktadır. 185 adet manzaralı ve konforlu odaları, geniş balo salonu, bölünebilen beş adet toplantı salonunun yanı sıra restoran ve lobi bar ile misafirlerine hizmet vermektedir.
+                                </p>
+                                <p><strong>Kongre merkezi ile arası yaklaşık 2 km mesafedir.</strong></p>
+                                <p>Rezervasyon ve fiyat bilgisi için: <a href="https://www.clariongoldenhorn.com" target="_blank">clariongoldenhorn.com</a></p>
+                            </div>
+                            <div class="mb-5">
+                                <h4 class="mb-3" style="color: #604c59;">RAMADA BY WYNDHAM OTEL</h4>
+                                <p>
+                                Ramada İstanbul'da yer alıyor. Muhteşem bir konuma sahip olan otelden, şehrin önemli alışveriş bölgelerine, iş merkezlerine ve Tarihi Şehir bölgesindeki gözde turistik alanlara kolayca ulaşabilirsiniz.
+                                </p>
+                                <p><strong>Kongre merkezi ile arası yaklaşık 3,5 km mesafedir.</strong></p>
+                                <p>Rezervasyon ve fiyat bilgisi için: <a href="https://ramadaalibeykoy.com/tr/" target="_blank">ramadaalibeykoy.com</a></p>
+                                </div>
+                            
+                            
+                            
+                            <!-- Mövenpick İstanbul Golden Horn -->
+                            <div class="mb-5">
+                                <h4 class="mb-3" style="color: #604c59;">Mövenpick İstanbul Golden Horn</h4>
+                                <p>
+                                    Mövenpick Hotel İstanbul Golden Horn; tarihi yarımada üzerinde, Haliç'te yer alır. Avrupa'yı Asya'ya bağlayan İstanbul'un ortasından geçen geniş bir su yolu olan ve Boğaziçi'nin devamı niteliğindeki Haliç kıyılarından tarih boyunca Bizans, Roma ve Osmanlı imparatorlukları yükselmiştir. Haliç, bugünün canlı, sofistike, zengin kültür çeşitliliğine sahip İstanbul'unun, varlık sebebi olan tarihi katmanlara ev sahipliği yapmıştır.
+                                </p>
+                                <p><strong>Kongre merkezi ile arası yaklaşık 2,5 km mesafedir.</strong></p>
+                                <p>Rezervasyon ve fiyat bilgisi için: <a href="https://www.movenpickistanbulgoldenhorn.com/tr/" target="_blank">movenpickistanbulgoldenhorn.com/</a></p>
+                            </div>
+                            
+                            <!-- Beyoğlu Öğretmenevi -->
+                            <div class="mb-5">
+                                <h4 class="mb-3" style="color: #604c59;">Beyoğlu Öğretmenevi</h4>
+                                <p><strong>Kongre merkezi ile arası yaklaşık 1 km mesafedir.</strong></p>
+                                <p>Rezervasyon ve fiyat bilgisi için: <a href="https://beyogluogretmenevi.meb.k12.tr/tema/index.php" target="_blank">beyogluogretmenevi.meb.k12.tr</a></p>
+                            </div>
                         </div>
                     </div>
                 </div>
