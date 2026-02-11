ea5080e66013b4e2e008f30a3dfec3fc0a6b3647
burakcelikkiran <80842957+burakcelikkiran@users.noreply.github.com>
2026-02-11 17:28:33 +0300

    Kurslar sayfası oluşturuldu. sayfada yer alan göz ikonlarına tıklandığında popup içerisinde kurs görselleri açıldı.

---
+++ kurslar.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="TR">
+    <?php $page = 'index'; ?>
+    <?php $pageTitle = 'Kurslar'; ?>
+
+	<?php include('head.php') ?>
+	<body class="homepage2-body">
+
+
+		<!--===== PAGE PROGRESS START=======-->
+		<div class="paginacontainer">
+			<div class="progress-wrap">
+				<svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+					<path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+				</svg>
+			</div>
+		</div>
+		<!--===== PAGE PROGRESS END=======-->
+        <?php include('header.php') ?>
+
+        <!--===== HERO AREA STARTS =======-->
+        <div class="hero2-slider-area" style="background-color: #604c58">
+            <div class="her2-section-area">
+                <img src="/assets/img/elements/elements9.png" alt="" class="elements9" />
+                <img src="/assets/img/elements/elements10.png" alt="" class="elements10" />
+                <img src="/assets/img/elements/elements11.png" alt="" class="elements11" />
+                <div class="img1">
+                    <img src="/doc/slider-01.png" alt="" />
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-2">
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="hero2-header">
+
+                                <img src="/doc/slider-01-metin.png" alt="" />
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+		<!--===== HERO AREA ENDS =======-->
+
+        <div class="team-sperkers-section-area sp1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 m-auto">
+                        <div class="heading2 text-center space-margin60">
+                            <h2> Kurslar</h2>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 m-auto">
+                        <div class="heading2 text-center space-margin60">
+                            <p>II. Uluslararası Onkoloji Hemşireliği Derneği Kongresi kapsamında düzenlenecek kurslar ve detayları aşağıda yer almaktadır. tablodaki göz ikonuna tıklayarak detaylarını inceleyebilirsiniz.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center w-100">
+                    <div class="col-md-12">
+                        <div class="our-team-boxarea">
+                            <div class="content-area">
+                                <table class="table table-bordered table-striped table-hover">
+                                    <colgroup>
+                                        <col span="1" style="width: 15%;">
+                                        <col span="1" style="width: 70%;">
+                                        <col span="1" style="width: 15%;">
+                                    </colgroup>
+                                    <thead style="background-color: #604c59; color: #ffffff;">
+                                        <tr>
+                                            <th style="vertical-align: middle;">Süre</th>
+                                            <th style="vertical-align: middle;">Kurs Adı</th>
+                                            <th style="vertical-align: middle; text-align: center;">Detay</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Temel Palyatif Bakım Hemşireliği Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/temel-palyatif-bakim-hemsireligi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Onkolojide Ağrı Yönetimi Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/onkolojide-agri-yonetimi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Onkoloji Hemşirelerini Güçlendirme; Mindfulness- Öz Şefkat -Nefes-Psikodrama Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/onkoloji-hemsirelerini-guclendirme-mindfulness-oz-sefkat-nefes-psikodrama.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Yarım gün</td>
+                                            <td>Kişilerarası İlişkiler Psikoterapisi (KİPT) Temelli Yaklaşımın Onkoloji Hemşireliğinde Kullanımı Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/kisilerarasi-iliskiler-psikoterapisi-kipt-temelli-yaklasimin-onkoloji-hemsireliginde-kullanimi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Pediatri Onkoloji Hemşireliğinde Kanıta Dayalı Uygulamalar Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/pediatri-onkoloji-hemsireliginde-kanita-dayali-uygulamalar-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Onkoloji Hemşireliğinde Temel Kanser Eğitimi Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/onkoloji-hemsireliginde-temel-kanser-egitimi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>İmmünoterapi Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/immunoterapi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Kanserde Tamamlayıcı ve Bütünleşik Sağlık Uygulamaları Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/kanserde-tamamlayici-ve-butunlesik-saglik-uygulamalari-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Meme Kanseri Yolculuğunu Anlamak: Sağ Kalımı Güçlendiren Bütüncül Bakım ve Uzun Yaşam Stratejileri Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/meme-kanseri-yolculugunu-anlamak-sagkalimi-guclendiren-butuncul-bakim-ve-uzun-yasam-stratejileri-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Radyasyon Onkolojisi Hemşireliğinde İnteraktif Beceri Eğitimi Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/radyasyon-onkolojisi-hemsireliginde-interaktif-beceri-egitimi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Temel Düzey Kök Hücre Nakli Hemşireliği Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/temel-duzey-kok-hucre-nakli-hemsireligi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Yarım gün</td>
+                                            <td>Terapötik Aferez Hemşireliği Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/terapotik-aferez-hemsireligi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Geriatrik Kanserlerde Kapsamlı Değerlendirme Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/geriatrik-kanserlerde-kapsamli-degerlendirme-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                        <tr>
+                                            <td>1 gün</td>
+                                            <td>Jineonkoloji Hemşireliği Kursu</td>
+                                            <td class="text-center course-detail-cell" data-image="/doc/posterler/jineonkoloji-hemsireligi-kursu.jpg"><img src="/doc/ico-show.svg" class="course-eye-icon" alt="Detay"></td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Course Modal -->
+        <div class="modal micromodal-slide" id="course-modal" aria-hidden="true">
+            <div class="modal__overlay" tabindex="-1" data-micromodal-close>
+                <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="course-modal-title" style="max-width: 800px;">
+                    <header class="modal__header" style="height: 20px;">
+                        <h2 class="modal__title" id="course-modal-title"></h2>
+                        <button class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+                    </header>
+                    <main class="modal__content" id="course-modal-content" style="margin-top: 1rem;">
+                        <img id="course-poster-img" src="" alt="Kurs Poster" style="width: 100%;">
+                    </main>
+                </div>
+            </div>
+        </div>
+
+        <?php include('footer.php') ?>
+        <?php include('script.php') ?>
+        <script src="https://unpkg.com/micromodal/dist/micromodal.min.js"></script>
+        <link rel="stylesheet" href="/assets/css/modal.css">
+        <style>
+            .course-eye-icon {
+                width: 24px;
+                height: 24px;
+                pointer-events: none;
+            }
+            .course-detail-cell {
+                cursor: pointer;
+                transition: background-color 0.2s;
+            }
+            .course-detail-cell:hover {
+                background-color: #f0f0f0;
+            }
+            #course-modal .modal__container {
+                max-width: 800px;
+            }
+        </style>
+        <script>
+            document.querySelectorAll('.course-detail-cell').forEach(cell => {
+                cell.addEventListener('click', function() {
+                    const imagePath = this.getAttribute('data-image');
+                    document.getElementById('course-poster-img').src = imagePath;
+                    MicroModal.show('course-modal', {
+                        openClass: 'is-open',
+                        disableScroll: true,
+                        disableFocus: false,
+                        awaitOpenAnimation: false,
+                        awaitCloseAnimation: false,
+                        debugMode: false
+                    });
+                });
+            });
+        </script>
+	</body>
+</html>
